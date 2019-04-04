42788b2dfe2a463e2e5cb2b4b0d4d3037cd136a6
Eric Weissman <eric.weissman21@gmail.com>
2019-04-04 17:13:01 -0600

    Adds Mutation Apollo component

--- src/components/Ratings/Ratings.js
+++ src/components/Ratings/Ratings.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from "graphql-tag";
 
 class Ratings extends Component {
   state = {
     loading: true,
     result: 3,
+    currClub: 0,
   }
 
   componentDidMount() {
-    //put Query in here?
+    const { club } = this.props
+    let currClub = 0
+    switch(club) {
+      case 'wedges':
+        currClub = 1
+        break
+      case 'irons':
+        currClub = 2
+        break
+      case 'woods':
+        currClub = 3
+        break
+    }
+    this.setState({currClub})
   }
 
+  
   updateResult = (e) => {
     const { value } = e.target
-    this.setState({result: parseInt(value)})
+    this.setState({ result: parseInt(value) })
+  }
+
+  getQuery = () => {
+    return {
+      query: `
+    mutation($clubId: Int!, $rating: Int!) {
+      createShot( clubId: $clubId, rating: $rating ) {
+        rating
+        clubId
+      }
+    }
+  `
+    }
+  }
+  makeMutation = () => {
+    const ADD_SHOT = gql`
+    mutation($clubId: Int!, $rating: Int!) {
+      createShot( clubId: $clubId, rating: $rating ) {
+        rating
+        clubId
+      }
+    }
+  `
+    const clubId = this.state.currClub
+    const rating = this.state.result
+    const { changeFilter } = this.props
+    return (
+      <Mutation mutation={ADD_SHOT} onCompleted={() => changeFilter('home')} variables={{ clubId, rating }}>
+        {addShot => (
+          <button className="save-shot" onClick={addShot}>Save Shot</button>
+        )}
+      </Mutation>
+    )
   }
 
   render() {
@@ -20,22 +70,24 @@ class Ratings extends Component {
 
     return (
       <div className="ratings">
-        <h2>Ratings</h2>
+        <h2>rate your shot</h2>
         <div className="ratings-area">
-            <div className="great-shot">
-              <button onClick={this.updateResult} value={3}>Great</button>
-            </div>
-            <div className="ok-shot">
-              <button onClick={this.updateResult} value={2}>Pull</button>
-              <button onClick={this.updateResult} value={4}>Push</button>
-            </div>
-            <div className="bad-shot">
-              <button onClick={this.updateResult} value={1}>Hook</button>
-              <button onClick={this.updateResult} value={6}>Duff</button>
-              <button onClick={this.updateResult} value={5}>Slice</button>
-            </div>
+          <div className="great-shot">
+            <button onClick={this.updateResult} value={3}>great</button>
+          </div>
+          <div className="ok-shot">
+            <button onClick={this.updateResult} value={2}>pull</button>
+            <button onClick={this.updateResult} value={4}>push</button>
+          </div>
+          <div className="bad-shot">
+            <button onClick={this.updateResult} value={1}>hook</button>
+            <button onClick={this.updateResult} value={6}>duff</button>
+            <button onClick={this.updateResult} value={5}>slice</button>
+          </div>
         </div>
-        <button onClick={() => changeFilter('home')}>Back</button>
+        {
+          this.makeMutation()
+        }
       </div>
     );
   }
