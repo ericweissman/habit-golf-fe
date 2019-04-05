64996f18357efe9855094f1a18486c42d1c097c7
Eric Weissman <eric.weissman21@gmail.com>
2019-04-05 07:29:34 -0600

    Adds redux and changes components to containers

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './components/App/App';
+import App from './containers/App/App';
 import ApolloClient from 'apollo-boost';
 import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { rootReducer } from '../src/reducers';
+import thunk from 'redux-thunk';
 
 const client = new ApolloClient({
   uri: "https://habit-golf-api.herokuapp.com/graphql"
 })
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  <Provider store={store}>
     <App />
-  </ApolloProvider>
+  </Provider>
   ,document.getElementById('root'));
+
+// ReactDOM.render(
+//   <ApolloProvider client={client}>
+//     <App />
+//   </ApolloProvider>
+//   ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
