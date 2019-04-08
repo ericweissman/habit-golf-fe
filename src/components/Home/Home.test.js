7c9bcfc8f42d8d917dd1a4488afa7a749f9dfef4
Eric Weissman <eric.weissman21@gmail.com>
2019-04-08 15:16:54 -0600

    Updates tests for Home and App

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -4,8 +4,35 @@ import { shallow } from 'enzyme'
 
 describe('Home', () => {
   let wrapper
+  let mockProps = {
+    changeClub: jest.fn(),
+    club: 'wedges',
+    changeFilter: jest.fn()
+  }
+  beforeEach(() => {
+    wrapper = shallow(<Home {...mockProps} />)
+  })
+
   it('should correctly match the snapshot', () => {
-    wrapper = shallow(<Home />)
+    wrapper = shallow(<Home {...mockProps} />)
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should have the correct default state', () => {
+    const expectedState = { currClub: 'wedges'}
+    expect(wrapper.state()).toEqual(expectedState)
+  })
+
+  it.skip('should call changeClub when changeClubs is called', () => {
+    const mockEvent = {target: {
+      value: 'irons'
+    }}
+    wrapper.instance().changeClubs(mockEvent)
+    expect(wrapper.props().changeClub).toHaveBeenCalled()
+  })
+  
+  it.skip('should call changeFilter once the track or view stats buttons are clicked', () => {
+    wrapper.find('.take-shot-btn').simulate('click')
+    expect(wrapper.props().changeFilter).toHaveBeenCalled()
   })
 })
--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,6 +1,94 @@
 import React from 'react';
-import App from './App';
+import { shallow } from 'enzyme'
+import  { App, mapStateToProps, mapDispatchToProps } from '../App/App';
+import { setActiveClub } from '../../actions'
+import { SignIn } from '../../components/SignIn/SignIn'
+import { NewUser } from '../../components/NewUser/NewUser'
 
-it('renders without crashing', () => {
-  expect(true).toEqual(true)
-});
+describe('App', () => {
+  let wrapper
+  const mockProps = {
+    isLoading: false,
+    error: '',
+    club: 'wedges',
+    setActiveClub: jest.fn()
+  }
+
+  it.skip('should correctly match the snapshot', () => {
+    wrapper = shallow(<App {...mockProps} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+
+  it('should have the proper default state', () => {
+    wrapper = shallow(<App {...mockProps} />)
+    const expectedState = {
+      user: [1],
+      filter: 'home',
+      newUser: false,
+    }
+    expect(wrapper.state()).toEqual(expectedState)
+  })
+
+
+  it('should toggle the newUser in state when toggleNewUser is called', () => {
+    const expectedState = {
+      newUser: true,
+      user: [1],
+      filter: 'home',
+    }
+    wrapper.instance().toggleNewUser()
+    expect(wrapper.state()).toEqual(expectedState)
+  })
+
+  it('should toggle filter when changeFilter is called', () => {
+    const expectedState = {
+      newUser: true,
+      user: [1],
+      filter: 'stats',
+    }
+    wrapper.instance().changeFilter('stats')
+    expect(wrapper.state()).toEqual(expectedState)
+  })
+
+  it('should call setActiveClub when changeClub is called', () => {
+    const mockClub = 'irons'
+    wrapper.instance().changeClub(mockClub)
+    expect(mockProps.setActiveClub).toHaveBeenCalled()
+  })
+
+  
+
+  describe('mapStateToProps', () => {
+    it('should return an object with isLoading, error, and club', () => {
+      const mockState = {
+        isLoading: false,
+        error: '',
+        club: 'wedges',
+        fakeState: 1,
+        fakeState2: 'a'
+      }
+
+      const expected = {
+        isLoading: false,
+        error: '',
+        club: 'wedges',
+      }
+
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('calls dispatch with the setActiveClub action', () => {
+      const mockDispatch = jest.fn()
+      const mockClub = 'irons'
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      const actionToDispatch = setActiveClub('irons')
+
+      mappedProps.setActiveClub(mockClub)
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    })
+  })
+})
