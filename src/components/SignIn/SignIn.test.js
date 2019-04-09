b6cf831aeaf0093bd253b5a9b4b4a24c92eb1fa6
Eric Weissman <eric.weissman21@gmail.com>
2019-04-09 09:02:21 -0600

    Adds tests for SignIn and adds files to ignore

--- src/components/SignIn/SignIn.test.js
+++ src/components/SignIn/SignIn.test.js
@@ -8,4 +8,31 @@ describe('SignIn', () => {
     wrapper = shallow(<SignIn />)
     expect(wrapper).toMatchSnapshot()
   })
+
+  it('should have the correct default state', () => {
+    const expectedState = {
+      email: '',
+      password: '',
+    }
+    expect(wrapper.state()).toEqual(expectedState)
+  })
+
+  it('should correctly update state based on input', () => {
+    const mockEvent1 = { target: { name: 'email', value: 'eric@geocities.com' } }
+    const mockEvent2 = { target: { name: 'password', value: 'abc' } }
+    const initialState = {
+      email: '',
+      password: '',
+    }
+    const expectedState = {
+      email: 'eric@geocities.com',
+      password: 'abc',
+    }
+
+    wrapper.setState(initialState)
+    wrapper.instance().handleChange(mockEvent1);
+    wrapper.instance().handleChange(mockEvent2);
+
+    expect(wrapper.state()).toEqual(expectedState)
+  })
 })
